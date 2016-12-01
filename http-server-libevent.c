0116d1e79cd078927b4d3f207eef568678fd2246
sunpeiyang <sunpeiyang0503@126.com>
2016-12-01 08:51:01 -0500

    libevent库写的 httpserver

---
+++ http-server-libevent.c
@@ -0,0 +1,326 @@
+/*
+ * g++ -g -o http http-server.c -levent
+ *
+ *
+  A trivial static http webserver using Libevent's evhttp.
+
+  This is not the best code in the world, and it does some fairly stupid stuff
+  that you would never want to do in a production webserver. Caveat hackor!
+
+ */
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#ifdef WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <io.h>
+#include <fcntl.h>
+#ifndef S_ISDIR
+#define S_ISDIR(x) (((x) & S_IFMT) == S_IFDIR)
+#endif
+#else
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#endif
+
+#include <event2/event.h>
+#include <event2/http.h>
+#include <event2/buffer.h>
+#include <event2/util.h>
+#include <event2/keyvalq_struct.h>
+
+#ifdef _EVENT_HAVE_NETINET_IN_H
+#include <netinet/in.h>
+# ifdef _XOPEN_SOURCE_EXTENDED
+#  include <arpa/inet.h>
+# endif
+#endif
+
+/* Compatibility for possible missing IPv6 declarations */
+#include "../util-internal.h"
+
+#ifdef WIN32
+#define stat _stat
+#define fstat _fstat
+#define open _open
+#define close _close
+#define O_RDONLY _O_RDONLY
+#endif
+using namespace std;
+char uri_root[512];
+
+static const struct table_entry {
+	const char *extension;
+	const char *content_type;
+} content_type_table[] = {
+	{ "txt", "text/plain" },
+	{ "c", "text/plain" },
+	{ "h", "text/plain" },
+	{ "html", "text/html" },
+	{ "htm", "text/htm" },
+	{ "css", "text/css" },
+	{ "gif", "image/gif" },
+	{ "jpg", "image/jpeg" },
+	{ "jpeg", "image/jpeg" },
+	{ "png", "image/png" },
+	{ "pdf", "application/pdf" },
+	{ "ps", "application/postsript" },
+	{ NULL, NULL },
+};
+
+/* Try to guess a good content-type for 'path' */
+static const char *
+guess_content_type(const char *path)
+{
+	const char *last_period, *extension;
+	const struct table_entry *ent;
+	last_period = strrchr(path, '.');
+	if (!last_period || strchr(last_period, '/'))
+		goto not_found; /* no exension */
+	extension = last_period + 1;
+	for (ent = &content_type_table[0]; ent->extension; ++ent) {
+		if (!evutil_ascii_strcasecmp(ent->extension, extension))
+			return ent->content_type;
+	}
+
+not_found:
+	return "application/misc";
+}
+
+static void
+post_request_cb(struct evhttp_request *req, void *arg)
+{
+	struct evbuffer *evb = NULL;
+	const char *docroot = (const char*)arg;
+	const char *uri = evhttp_request_get_uri(req);
+	struct evhttp_uri *decoded = NULL;
+	const char *path;
+	char *decoded_path;
+	size_t len;
+
+
+	printf("Got a Post request for <%s>\n",  uri);
+
+	/* Decode the URI */
+	decoded = evhttp_uri_parse(uri);
+	if (!decoded) {
+		printf("It's not a good URI. Sending BADREQUEST\n");
+		evhttp_send_error(req, HTTP_BADREQUEST, 0);
+		return;
+	}
+	/* Let's see what path the user asked for. */
+	path = evhttp_uri_get_path(decoded);
+	if (!path) path = "/";
+
+	/* We need to decode it, to see what path the user really wanted. */
+	decoded_path = evhttp_uridecode(path, 0, NULL);
+		
+	evb = evhttp_request_get_input_buffer(req);
+	char post_content[100];
+	string ss = "";
+	int length = evbuffer_get_length(evb);
+	printf("length=%d", length);
+
+	char* des = (char*)malloc(length);
+	evbuffer_remove(evb, des, length);	
+
+#if 0
+	while (evbuffer_get_length(evb))
+	{
+		int n;
+		char cbuf[128];
+		n = evbuffer_remove(evb, cbuf, sizeof(cbuf));
+		printf("n=%d, cbuf=%s \n", n, cbuf);
+//		if (n > 0)
+			strncat(post_content, cbuf, n);
+		ss += cbuf;
+	}
+#endif
+	printf("des=%s\n", des);
+	//printf("post_content: %s\n", post_content);
+	/* This holds the content we're sending. */
+	printf("before reply\n");
+	evhttp_send_reply(req, 200, "OK", NULL);
+
+	if (decoded)
+		evhttp_uri_free(decoded);
+	if (decoded_path)
+		free(decoded_path);
+}
+
+/* This callback gets invoked when we get any http request that doesn't match
+ * any other callback.  Like any evhttp server callback, it has a simple job:
+ * it must eventually call evhttp_send_error() or evhttp_send_reply().
+ */
+static void
+send_document_cb(struct evhttp_request *req, void *arg)
+{
+	struct evbuffer *evb = NULL;
+	const char *docroot = (const char*)arg;
+	const char *uri = evhttp_request_get_uri(req);
+	struct evhttp_uri *decoded = NULL;
+	const char *path;
+	char *decoded_path;
+	char *whole_path = NULL;
+	size_t len;
+	int fd = -1;
+	struct stat st;
+
+	int req_flag = 0;
+	if (evhttp_request_get_command(req) != EVHTTP_REQ_GET) {
+		post_request_cb(req, arg);	
+		return;
+	}
+
+
+	printf("Got a GET request for <%s>\n",  uri);
+
+	/* Decode the URI */
+	decoded = evhttp_uri_parse(uri);
+	if (!decoded) {
+		printf("It's not a good URI. Sending BADREQUEST\n");
+		evhttp_send_error(req, HTTP_BADREQUEST, 0);
+		return;
+	}
+	/* Let's see what path the user asked for. */
+	path = evhttp_uri_get_path(decoded);
+	if (!path) path = "/";
+
+	/* We need to decode it, to see what path the user really wanted. */
+	decoded_path = evhttp_uridecode(path, 0, NULL);
+	/* Don't allow any ".."s in the path, to avoid exposing stuff outside
+	 * of the docroot.  This test is both overzealous and underzealous:
+	 * it forbids aceptable paths like "/this/one..here", but it doesn't
+	 * do anything to prevent symlink following." */
+	char* req_content;
+	if (strstr(decoded_path, "sun"))
+		req_content = "sun pei yang .";
+		
+	/* This holds the content we're sending. */
+	evb = evbuffer_new();
+
+	evbuffer_add_printf(evb, "<html>\n <head>\n"
+		    "  <title>sun</title>\n"
+		    " </head>\n"
+		    " <body>\n"
+		    "  <h1>%s</h1>\n"
+		    ,path);
+	evbuffer_add_printf(evb, "</body></html>\n");
+	evhttp_add_header(evhttp_request_get_output_headers(req),
+		    "Content-Type", "text/html");
+
+	evhttp_send_reply(req, 200, "OK", evb);
+	goto done;
+err:
+	evhttp_send_error(req, 404, "Document was not found");
+	if (fd>=0)
+		close(fd);
+done:
+	if (decoded)
+		evhttp_uri_free(decoded);
+	if (decoded_path)
+		free(decoded_path);
+	if (whole_path)
+		free(whole_path);
+	if (evb)
+		evbuffer_free(evb);
+}
+
+int
+main(int argc, char **argv)
+{
+	struct event_base *base;
+	struct evhttp *http;
+	struct evhttp_bound_socket *handle;
+
+	unsigned short port = 8181;
+#ifdef WIN32
+	WSADATA WSAData;
+	WSAStartup(0x101, &WSAData);
+#else
+	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
+		return (1);
+#endif
+
+	base = event_base_new();
+	if (!base) {
+		fprintf(stderr, "Couldn't create an event_base: exiting\n");
+		return 1;
+	}
+
+	/* Create a new evhttp object to handle requests. */
+	http = evhttp_new(base);
+	if (!http) {
+		fprintf(stderr, "couldn't create evhttp. Exiting.\n");
+		return 1;
+	}
+
+	/* The /dump URI will dump all requests to stdout and say 200 ok. */
+//	evhttp_set_cb(http, "/dump", dump_request_cb, NULL);
+
+	/* We want to accept arbitrary requests, so we need to set a "generic"
+	 * cb.  We can also add callbacks for specific paths. */
+	evhttp_set_gencb(http, send_document_cb, NULL);
+
+	/* Now we tell the evhttp what port to listen on */
+	handle = evhttp_bind_socket_with_handle(http, "192.168.62.107", port);
+	if (!handle) {
+		fprintf(stderr, "couldn't bind to port %d. Exiting.\n",
+		    (int)port);
+		return 1;
+	}
+#if 0
+	{
+		/* Extract and display the address we're listening on. */
+		struct sockaddr_storage ss;
+		evutil_socket_t fd;
+		ev_socklen_t socklen = sizeof(ss);
+		char addrbuf[128];
+		void *inaddr;
+		const char *addr;
+		int got_port = -1;
+		fd = evhttp_bound_socket_get_fd(handle);
+		memset(&ss, 0, sizeof(ss));
+		if (getsockname(fd, (struct sockaddr *)&ss, &socklen)) {
+			perror("getsockname() failed");
+			return 1;
+		}
+		if (ss.ss_family == AF_INET) {
+			got_port = ntohs(((struct sockaddr_in*)&ss)->sin_port);
+			inaddr = &((struct sockaddr_in*)&ss)->sin_addr;
+		} else if (ss.ss_family == AF_INET6) {
+			got_port = ntohs(((struct sockaddr_in6*)&ss)->sin6_port);
+			inaddr = &((struct sockaddr_in6*)&ss)->sin6_addr;
+		} else {
+			fprintf(stderr, "Weird address family %d\n",
+			    ss.ss_family);
+			return 1;
+		}
+		addr = evutil_inet_ntop(ss.ss_family, inaddr, addrbuf,
+		    sizeof(addrbuf));
+		if (addr) {
+			printf("Listening on %s:%d\n", addr, got_port);
+			evutil_snprintf(uri_root, sizeof(uri_root),
+			    "http://%s:%d",addr,got_port);
+		} else {
+			fprintf(stderr, "evutil_inet_ntop failed\n");
+			return 1;
+		}
+	}
+#endif
+	event_base_dispatch(base);
+
+	return 0;
+}
