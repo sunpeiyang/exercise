acef7602edc82843cd27dbe54bd5dd6fd98591cf
sunpeiyang <sunpeiyang0503@126.com>
2016-12-06 11:25:31 -0500

    添加icebox demo

---
+++ icebox/HelloServiceI.cpp
@@ -0,0 +1,48 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2016 ZeroC, Inc. All rights reserved.
+//
+// **********************************************************************
+
+#include <Ice/Ice.h>
+#include <HelloServiceI.h>
+#include <HelloI.h>
+
+using namespace std;
+
+extern "C"
+{
+
+//
+// Factory function
+//
+ICE_DECLSPEC_EXPORT IceBox::Service*
+create(Ice::CommunicatorPtr)
+{
+    return new HelloServiceI;
+}
+
+}
+
+HelloServiceI::HelloServiceI()
+{
+}
+
+HelloServiceI::~HelloServiceI()
+{
+}
+
+void
+HelloServiceI::start(const string& name, const Ice::CommunicatorPtr& communicator, const Ice::StringSeq& /*args*/)
+{
+    _adapter = communicator->createObjectAdapter(name);
+    Demo::HelloPtr hello = new HelloI;
+    _adapter->add(hello, communicator->stringToIdentity("hello"));
+    _adapter->activate();
+}
+
+void
+HelloServiceI::stop()
+{
+    _adapter->destroy();
+}
---
+++ icebox/HelloServiceI.h
@@ -0,0 +1,30 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2016 ZeroC, Inc. All rights reserved.
+//
+// **********************************************************************
+
+#ifndef HELLO_SERVICE_I_H
+#define HELLO_SERVICE_I_H
+
+#include <IceBox/IceBox.h>
+
+class HelloServiceI : public ::IceBox::Service
+{
+public:
+
+    HelloServiceI();
+    virtual ~HelloServiceI();
+
+    virtual void start(const ::std::string&,
+                       const ::Ice::CommunicatorPtr&,
+                       const ::Ice::StringSeq&);
+
+    virtual void stop();
+
+private:
+
+    ::Ice::ObjectAdapterPtr _adapter;
+};
+
+#endif
